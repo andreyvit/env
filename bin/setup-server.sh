f7cbe8f772816cb02bf8526ce2ff1574ca028a23
Andrey Tarantsov <andrey@tarantsov.com>
2017-07-27 20:50:45 +0700

    setup-server

--- bin/setup-server.sh
+++ bin/setup-server.sh
@@ -10,7 +10,7 @@ fi
 # apt-get update
 apt-get dist-upgrade -y
 
-apt-get install -y fail2ban unattended-upgrades logwatch zsh
+apt-get install -y fail2ban unattended-upgrades logwatch zsh direnv pv mosh
 perl -i -pe 's!^/usr/sbin/logwatch .*!/usr/sbin/logwatch --output mail --mailto andrey@tarantsov.com --detail high!' /etc/cron.daily/00logwatch
 
 perl -i -pe 's/\) ALL/) NOPASSWD:ALL/' /etc/sudoers
@@ -23,22 +23,17 @@ grep -q '^IPV6=yes$' /etc/default/ufw
 ufw allow 22
 ufw allow 80
 ufw allow 443
+ufw allow 60000:61000/udp
 ufw --force enable
 
 apt-get install -y build-essential git screen tree
 
 apt-get install -y libglu1-mesa libxi-dev libxmu-dev libopenblas-dev liblapack-dev gfortran
-apt-get install -y python-pip python-dev python-numpy python-scipy
+apt-get install -y python-pip python-dev python-numpy python-scipy virtualenv
+# apt-get install -y python2-dev python2-pip python2-virtualenv
 
 # Instructions from:
-#     http://www.linuxandubuntu.com/home/how-to-install-latest-nvidia-drivers-in-linux
-# For the latest version number, check under "## Current Status" on:
-#     https://launchpad.net/~graphics-drivers/+archive/ubuntu/ppa
-# Before upgrading the number here, you need to manually run:
-#     apt-get purge 'nvidia*'
-# Check installation:
-#     lsmod | grep nvidia
-# apt-get install -y nvidia-381
+#     https://github.com/lwneal/install-keras
 
 if ! nvidia-smi; then
     echo "Sadly, you need to install NVIDIA drivers yourself:"
@@ -68,6 +63,16 @@ if ! test -d /usr/local/cuda-8.0/bin; then
     exit 1
 fi
 
+if ! find /usr/lib | grep -q libcudnn; then
+    echo "Sadly, you need to install CuDNN yourself (after registering with NVIDIA):"
+    echo
+    echo "    https://developer.nvidia.com/cudnn"
+fi
+
+if ! which go; then
+    wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.8.3.linux-amd64.tar.gz
+fi
 
 ################################################################################
 # User: andreyvit
@@ -76,10 +81,12 @@ test -d /home/andreyvit || adduser --disabled-password --gecos "Andrey Tarantsov
 usermod -aG sudo andreyvit
 
 mkdir -p /home/andreyvit/.ssh
-test -f /home/andreyvit/.ssh/authorized_keys || echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+0QKFkwIsWe1M2vGpC7muP18C1BcYd8q2b0p/DQSjW2eT43/TnzZk2OQa8d4AfZjnkEYjZOCVYQF4/U0+EQsHW45c+SX2mNIDdU/IpR/F/GtvJogJEFvib+mIYp2ED9N4IYiwHqS0ygI32pmObsPTa0j3AsO0ExuOzseE7cNPFy0iDxj85gT+zp/D5pZcG5n64UwAyyGHrzHwGsOfK6EjgBVpD3Y5PqLS/4krB3ynSeSptmPLzP/v+tWgLTxYAUlcxr9p7JrC2fBYDlbFxE/7pyryrguNf4IkftQLHJWz3PQ++wNCeIqFRBeQoWBbhOOwF4ZNNArSLEuJFzM+WzKBpkHzRtMifJCDx3CNs8YeSLsAvtU/VEoouH1Cco4rtfiuwsCBcNM25d7eWSnG4h9loTTq4AAWxHddQ4KKGiKnlpBn3/W80XG1LPzMuWoXbYn7gxHiEtGz9IXw/ubvNn/96esfXinzwky0Zl4r1LS61XEakRTmoyO2UXWztePdqPRU9yC2fMbAFbWJDi2s/BvbKlVvH/7w4OpV2CXv5BG/vihBzXXpixBN0x0pZz2pdUE9y9FPGGI/r22Zu6OOr2O6kWbC2hlDKtYLVGavknXNS3/7k4VFICH6wKHk9XOLT52rklt3399bQAZ3t7y5x4HoHvt2+GXOsH6GV3aaRMZ49Q== andreyvit@imac2016jun" >> /home/andreyvit/.ssh/authorized_keys
+test -f /home/andreyvit/.ssh/authorized_keys && grep -q 'andreyvit@imac2016jun' /home/andreyvit/.ssh/authorized_keys || echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+0QKFkwIsWe1M2vGpC7muP18C1BcYd8q2b0p/DQSjW2eT43/TnzZk2OQa8d4AfZjnkEYjZOCVYQF4/U0+EQsHW45c+SX2mNIDdU/IpR/F/GtvJogJEFvib+mIYp2ED9N4IYiwHqS0ygI32pmObsPTa0j3AsO0ExuOzseE7cNPFy0iDxj85gT+zp/D5pZcG5n64UwAyyGHrzHwGsOfK6EjgBVpD3Y5PqLS/4krB3ynSeSptmPLzP/v+tWgLTxYAUlcxr9p7JrC2fBYDlbFxE/7pyryrguNf4IkftQLHJWz3PQ++wNCeIqFRBeQoWBbhOOwF4ZNNArSLEuJFzM+WzKBpkHzRtMifJCDx3CNs8YeSLsAvtU/VEoouH1Cco4rtfiuwsCBcNM25d7eWSnG4h9loTTq4AAWxHddQ4KKGiKnlpBn3/W80XG1LPzMuWoXbYn7gxHiEtGz9IXw/ubvNn/96esfXinzwky0Zl4r1LS61XEakRTmoyO2UXWztePdqPRU9yC2fMbAFbWJDi2s/BvbKlVvH/7w4OpV2CXv5BG/vihBzXXpixBN0x0pZz2pdUE9y9FPGGI/r22Zu6OOr2O6kWbC2hlDKtYLVGavknXNS3/7k4VFICH6wKHk9XOLT52rklt3399bQAZ3t7y5x4HoHvt2+GXOsH6GV3aaRMZ49Q== andreyvit@imac2016jun" >> /home/andreyvit/.ssh/authorized_keys
+test -f /home/andreyvit/.ssh/known_hosts && grep -q 'github.com' /home/andreyvit/.ssh/known_hosts || echo "github.com,192.30.253.113 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >>/home/andreyvit/.ssh/known_hosts
 chown -R andreyvit:andreyvit /home/andreyvit/.ssh
 chmod 700 /home/andreyvit/.ssh
 chmod 600 /home/andreyvit/.ssh/authorized_keys
+chmod 644 /home/andreyvit/.ssh/known_hosts
 
 if ! test -f /home/andreyvit/.ssh/id_rsa; then
     sudo -Hu andreyvit ssh-keygen -q -t rsa -b 4096 -C 'andreyvit@grief' -N "" -f /home/andreyvit/.ssh/id_rsa
@@ -92,7 +99,7 @@ if ! test -f /home/andreyvit/.ssh/id_rsa; then
     read -p "Press ENTER when ready: "
 fi
 
-test -d /home/andreyvit/.oh-my-zsh || sudo -Hu andreyvit sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+test -d /home/andreyvit/.oh-my-zsh || sudo -Hu andreyvit sh -c "cd; $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cd /home/andreyvit
 test -d /home/andreyvit/env || sudo -Hu andreyvit bash -c 'cd; git clone git@github.com:andreyvit/env.git'
